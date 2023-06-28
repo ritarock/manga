13b58c7952c6ff3854024c61a112be2bb9b2a195
ritarock <dvryz1204@gmail.com>
2023-06-28 12:11:20 +0900

    fix

--- Makefile
+++ Makefile
@@ -2,14 +2,11 @@
 
 BINDIR=bin
 
-gqlgen:
-	go run github.com/99designs/gqlgen
-
 build:
 	go build -o $(BINDIR)/manga .
 
 test:
-	go test $(shell go list ./... | grep internal)
+	go test ./...
 
 install:
 	go install
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -9,13 +6,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use: "manga",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	err := rootCmd.Execute()
